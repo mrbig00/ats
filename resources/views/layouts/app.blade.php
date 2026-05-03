fc8d7430f9d187d3e39f7ea0e187403c55350925
Szanto Zoltan <mrbig00@gmail.com>
2026-05-03 16:46:09 +0300

    fix version indicator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <x-layouts::app.sidebar :title="$title ?? null">
     <flux:main>
-        {{ $slot }}
+        {{-- Block wrapper: shells use flex-1; prevents them stealing flex height so the version line sits below page content instead of at the viewport bottom. --}}
+        <div class="w-full min-w-0 shrink-0 grow-0 basis-auto">
+            {{ $slot }}
+        </div>
         <x-app-version-indicator />
     </flux:main>
 </x-layouts::app.sidebar>
